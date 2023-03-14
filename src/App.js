2f9b3021153ba76776afbdae1d9a99e9afe827d7
jdepstein <jdepstein2020@gmail.com>
2023-03-14 17:12:12 -0400

    small error causing no deplot

--- src/App.js
+++ src/App.js
@@ -4,40 +4,13 @@ import Tuiter from "./tuiter";
 import {BrowserRouter, Link} from "react-router-dom";
 import {Routes, Route} from "react-router";
 import Nav from "./nav";
-import {useState} from "react";
-import Rew from "./TEST/Rew";
-import Asd from "./TEST/Asd";
 
 
 
 
 
 function App() {
-        /*
-             const [qwe, wer] = useState([123, 234, 345, 456, 567])
-        const ert = (rty) => {
-            const tyu = qwe.filter(yui => yui != 345)
-            wer(tyu)
-        }
-                 */
     return (
-    /*
-        <div>
-            <ul>
-                {qwe.map(uio => <li>{uio}</li>)}
-            </ul>
-            <button onClick={() => ert(qwe)}>BUTTON</button>
-        </div>
-
-        <BrowserRouter>
-            <Link to="/qwe/21/14/wer">LINK A</Link>
-            <Routes>
-                <Route path="/qwe/:qwe/:wer/wer" element={<Rew/>}/>
-                <Route path="/asd/:zxc/:xcv" element={<Asd/>}/>
-            </Routes>
-        </BrowserRouter>
-        */
-
         <BrowserRouter>
             <div className="container">
                 <Routes>
